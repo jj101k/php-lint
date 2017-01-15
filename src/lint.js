87f408db72a18df46646c2c1943eb60179404142
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-01-15 14:19:01 +0000

    Change into more of an object-based system

--- src/lint.js
+++ src/lint.js
@@ -9,37 +9,8 @@ class Lint {
             }
         );
     }
-    get nodeHandlers() {
-        return {
-            assign: node => {
-                this.namespace.push(node.left.name);
-                this.checkTypedNode(node.right);
-            },
-            echo: node => {
-                node.arguments.forEach(n => this.checkTypedNode(n));
-            },
-            number: () => {},
-            program: node => {
-                node.children.forEach(n => this.checkTypedNode(n));
-            },
-            variable: node => {
-                if(!this.namespace.find(name => name == node.name)) {
-                    throw new Lint.PHPStrictError(`Name ${node.name} is not defined in this namespace`);
-                }
-            },
-            string: () => {},
-        };
-    }
     check() {
-        this.checkTypedNode(this.tree);
-        return true;
-    }
-    checkTypedNode(node) {
-        if(this.nodeHandlers[node.kind]) {
-            return this.nodeHandlers[node.kind](node);
-        } else {
-            throw new Error(`Don't know how to handle nodes of type ${node.kind}`);
-        }
+        return Lint.ShadowTree.Node.typed(this.tree).check();
     }
     static check(tree, filename = null) {
         var l = new Lint(tree, filename);
@@ -49,4 +20,113 @@ class Lint {
 
 Lint.PHPStrictError = class extends Error {
 };
+Lint.ShadowTree = {
+    Node: class {
+        /** @property {Location|null} loc
+          * @property {string} kind
+          */
+        constructor(node) {
+            Object.assign(
+                this,
+                node
+            );
+        }
+        check(ns) {
+            return true;
+        }
+        static typed(node) {
+            var c = Lint.ShadowTree[node.constructor.name];
+            if(!c) {
+                throw new Error(`No handler for ${node.constructor.name}`);
+            }
+            return new c(node);
+        }
+    },
+};
+Object.assign(
+    Lint.ShadowTree,
+    {
+        Statement: class extends Lint.ShadowTree.Node {
+        },
+        Expression: class extends Lint.ShadowTree.Node {
+        },
+    }
+);
+Object.assign(
+    Lint.ShadowTree,
+    {
+        Assign: class extends Lint.ShadowTree.Statement {
+            /** @property {Expression} left
+              * @property {Expression} right
+              * @property {string} operator
+              */
+            constructor(node) {
+                super(node);
+                this.left = Lint.ShadowTree.Node.typed(node.left);
+                this.right = Lint.ShadowTree.Node.typed(node.right);
+            }
+            check(ns) {
+                ns.push(this.left.name);
+                this.right.check(ns);
+                return super.check(ns);
+            }
+        },
+        Block: class extends Lint.ShadowTree.Statement {
+            /** @property {Node[]} children */
+            constructor(node) {
+                super(node);
+                this.children = node.children.map(child => Lint.ShadowTree.Node.typed(child));
+            }
+        },
+        Literal: class extends Lint.ShadowTree.Expression {
+            /** @property {Node|string|number|boolean|null} value */
+        },
+        Sys: class extends Lint.ShadowTree.Statement {
+            /** @property {Node[]} arguments */
+            constructor(node) {
+                super(node);
+                this.arguments = node.arguments.map(child => Lint.ShadowTree.Node.typed(child));
+            }
+        },
+        Variable: class extends Lint.ShadowTree.Expression {
+            /** @property {string|Node} name
+              * @property {bool} byref
+              */
+            constructor(node) {
+                super(node);
+                if(typeof this.name == "object") {
+                    this.name = Lint.ShadowTree.Node.typed(child);
+                }
+            }
+            check(ns) {
+                if(!ns.find(name => name == this.name)) {
+                    throw new Lint.PHPStrictError(`Name \$${this.name} is not defined in this namespace`);
+                }
+                return super.check(ns);
+            }
+        },
+    }
+);
+Object.assign(
+    Lint.ShadowTree,
+    {
+        Echo: class extends Lint.ShadowTree.Sys {
+            check(ns) {
+                this.arguments.forEach(child => child.check(ns));
+                return super.check(ns);
+            }
+        },
+        Program: class extends Lint.ShadowTree.Block {
+            /** @property {Error[]} errors */
+            check(ns) {
+                var ns_inner = [];
+                this.children.forEach(child => child.check(ns_inner));
+                return super.check(ns);
+            }
+        },
+        String: class extends Lint.ShadowTree.Literal {
+            /** @property {string} label */
+        },
+    }
+);
 module.exports = Lint;
