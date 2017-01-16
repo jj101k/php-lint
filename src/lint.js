16a433c2530ab7db2cab4310629a3768e7474b2b
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-01-16 22:31:36 +0000

    General improvements

--- src/lint.js
+++ src/lint.js
@@ -1,3 +1,4 @@
+"use strict";
 class Lint {
     constructor(tree, filename = null, namespace = []) {
         Object.assign(
@@ -10,7 +11,7 @@ class Lint {
         );
     }
     check() {
-        return Lint.ShadowTree.Node.typed(this.tree).check();
+        return Lint.ShadowTree.Node.typed(this.tree).check([]);
     }
     static check(tree, filename = null) {
         var l = new Lint(tree, filename);
@@ -39,6 +40,22 @@ Lint.ShadowTree = {
         get loc() {
             return this.node.loc;
         }
+        cacheNode(name) {
+            return this.cacheProperty(
+                name,
+                subnode => subnode ?
+                    Lint.ShadowTree.Node.typed(subnode) :
+                    subnode
+            );
+        }
+        cacheNodeArray(name) {
+            return this.cacheProperty(
+                name,
+                subnodes => subnodes ? subnodes.map(
+                    subnode => Lint.ShadowTree.Node.typed(subnode)
+                ) : subnodes
+            );
+        }
         cacheProperty(name, f) {
             if(!this._cache.hasOwnProperty(name)) {
                 this._cache[name] = f(this.node[name]);
@@ -60,9 +77,20 @@ Lint.ShadowTree = {
 Object.assign(
     Lint.ShadowTree,
     {
-        Statement: class extends Lint.ShadowTree.Node {
-        },
         Expression: class extends Lint.ShadowTree.Node {
+        },
+        Identifier: class extends Lint.ShadowTree.Node {
+            /** @property {string} name */
+            get name() {
+                return this.node.name;
+            }
+            /** @property {string} resolution One of UNQUALIFIED_NAME,
+              *     QUALIFIED_NAME, FULL_QUALIFIED_NAME or RELATIVE_NAME */
+            get resolution() {
+                return this.node.resolution;
+            }
+        },
+        Statement: class extends Lint.ShadowTree.Node {
         },
     }
 );
@@ -76,11 +104,11 @@ Object.assign(
             }
             /** @property {Expression} left */
             get left() {
-                return this.cacheProperty("left", subnode => Lint.ShadowTree.Node.typed(subnode))
+                return this.cacheNode("left")
             }
             /** @property {Expression} right */
             get right() {
-                return this.cacheProperty("right", subnode => Lint.ShadowTree.Node.typed(subnode))
+                return this.cacheNode("right")
             }
             check(ns) {
                 ns.push(this.left.name);
@@ -91,10 +119,11 @@ Object.assign(
         Block: class extends Lint.ShadowTree.Statement {
             /** @property {Node[]} children */
             get children() {
-                return this.cacheProperty(
-                    "children",
-                    nodes => nodes.map(child => Lint.ShadowTree.Node.typed(child))
-                );
+                return this.cacheNodeArray("children");
+            }
+            check(ns) {
+                this.children.forEach(node => node.check(ns));
+                return super.check(ns);
             }
         },
         Literal: class extends Lint.ShadowTree.Expression {
@@ -103,10 +132,7 @@ Object.assign(
         Sys: class extends Lint.ShadowTree.Statement {
             /** @property {Node[]} arguments */
             get arguments() {
-                return this.cacheProperty(
-                    "arguments",
-                    nodes => nodes.map(child => Lint.ShadowTree.Node.typed(child))
-                );
+                return this.cacheNodeArray("arguments");
             }
         },
         Variable: class extends Lint.ShadowTree.Expression {
--- test/test-all.js
+++ test/test-all.js
@@ -1,39 +1,46 @@
 var phpLint = require("../src/index");
+var glob = require("glob");
 
 var good_code = "<?php $foo = '1234'; echo $foo;";
-var good_file = "test/good-sample.php";
+var good_files = glob.sync('test/good-*.php');
 var bad_code = "<?php echo $foo;";
-var bad_file = "test/bad-sample.php";
+var bad_files = glob.sync('test/bad-*.php');
 
 exports["test async"] = (assert, done) => {
-    return Promise.all([
-        phpLint.checkSourceCode(good_code).then(
-            result => assert.ok(result, "Valid code looks ok")
-        ),
-        phpLint.checkFile(good_file).then(
-            result => assert.ok(result, "Valid file looks ok")
-        ),
-        phpLint.checkSourceCode(bad_code).catch(
-            error => assert.ok(error, "Invalid code looks bad")
-        ),
-        phpLint.checkFile(bad_file).catch(
-            error => assert.ok(error, "Invalid file looks bad")
-        ),
-    ]).then(() => {
+    return Promise.all(
+        [
+            phpLint.checkSourceCode(good_code).then(
+                result => assert.ok(result, "Valid code looks ok")
+            ),
+            phpLint.checkSourceCode(bad_code).catch(
+                error => assert.ok(error, "Invalid code looks bad")
+            ),
+        ].concat(
+            bad_files.map(bad_file => phpLint.checkFile(bad_file).catch(
+                error => assert.ok(error, `Invalid file ${bad_file} looks bad`)
+            ))
+        ).concat(
+            good_files.map(good_file => phpLint.checkFile(good_file).then(
+                result => assert.ok(result, `Valid file ${good_file} looks ok`)
+            ))
+        )
+    ).then(() => {
         done();
     });
 };
 exports["test sync"] = (assert) => {
     var result = phpLint.checkSourceCodeSync(good_code);
     assert.ok(result, "Valid code looks ok");
-    var result = phpLint.checkFileSync(good_file);
-    assert.ok(result, "Valid file looks ok");
+    good_files.forEach(good_file => assert.ok(
+        phpLint.checkFileSync(good_file),
+        `Valid file ${good_file} looks ok`
+    ));
     assert.throws(() => {
         var result = phpLint.checkSourceCodeSync(bad_code);
     }, "Invalid code looks bad");
-    assert.throws(() => {
+    bad_files.forEach(bad_file => assert.throws(() => {
         var result = phpLint.checkFileSync(bad_file);
-    }, "Invalid file looks bad");
+    }, `Invalid file ${bad_file} looks bad`));
 };
 
 if (module == require.main) require('test').run(exports)
