499759826c0fc89876247b91e79b3adfa50c5c16
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-07-09 16:32:07 +0100

    Add basic support for array lookups

--- src/lib/shadowtree.js
+++ src/lib/shadowtree.js
@@ -1545,6 +1545,48 @@ class Nowdoc extends Literal {
     }
 }
 class OffsetLookup extends Lookup {
+    /**
+     * Checks that syntax seems ok
+     * @param {Context} context
+     * @param {boolean} [in_call]
+     * @returns {?PHPTypeUnion} The set of types applicable to this value
+     */
+    check(context, in_call = false) {
+        super.check(context)
+        let types_in
+        if(
+            this.what instanceof Variable ||
+            this.what instanceof PropertyLookup ||
+            this.what instanceof StaticLookup ||
+            this.what instanceof OffsetLookup ||
+            this.what instanceof Parenthesis
+        ) {
+            let inner_context = context.childContext(true)
+            inner_context.isAssigning = false
+            types_in = this.what.check(inner_context)
+        } else if(
+            this.what instanceof Call
+        ) {
+            let type_union = this.what.check(context)
+            types_in = PHPTypeUnion.empty
+            type_union.types.forEach(t => {
+                if(t instanceof PHPFunctionType) {
+                    types_in.addType(t.returnType)
+                } else {
+                    types_in.addTypesFrom(PHPTypeUnion.mixed)
+                }
+            })
+        } else {
+            console.log(this.node)
+            console.log("TODO don't know how to check this kind of lookup")
+            return PHPTypeUnion.mixed
+        }
+        if(this.offset instanceof Variable) {
+            return PHPTypeUnion.mixed
+        } else {
+            return PHPTypeUnion.mixed // TODO improve
+        }
+    }
 }
 class Parenthesis extends Operation {
     /** @type {Expression} */
