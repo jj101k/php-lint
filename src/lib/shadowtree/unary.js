f47ac84a94d0348313a6954a95f0702350d3de67
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-08-20 15:41:32 +0100

    Add a unary operator (!)

--- src/lib/shadowtree/unary.js
+++ src/lib/shadowtree/unary.js
@@ -3,6 +3,7 @@ import ContextTypes from "../context-types"
 import Operation from "./operation"
 import Expression from "./expression"
 import Doc from "./doc"
+import {PHPSimpleType} from "../phptype"
 export default class Unary extends Operation {
     /** @type {string} */
     get type() {
@@ -21,6 +22,27 @@ export default class Unary extends Operation {
      */
     check(context, in_call = false, doc = null) {
         super.check(context, in_call, doc)
-        return this.what.check(context, false, null) // FIXME infer from type
+        if(this.type == "!") {
+            let etype = this.what.check(context, false, null).expressionType
+            let etype_single
+            if(
+                etype.types.length == 1 &&
+                (etype_single = etype.types[0]) &&
+                etype_single instanceof PHPSimpleType &&
+                etype_single.values.length > 0
+            ) {
+                let possible_values = {}
+                etype_single.values.forEach(
+                    v => possible_values[+!v] = !v
+                )
+                if(Object.keys(possible_values).length == 1) {
+                    return new ContextTypes(PHPSimpleType.coreTypes.bool.withValue(Object.values(possible_values)[0]))
+                }
+            }
+            return new ContextTypes(PHPSimpleType.coreTypes.bool)
+        } else {
+            console.log(`TODO: Unary type ${this.type} not handled yet`)
+            return this.what.check(context, false, null)
+        }
     }
 }
