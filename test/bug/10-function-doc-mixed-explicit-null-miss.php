c060b4246f70e9e628191a1fab7b310dbbfe2d2a
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-04-03 11:44:24 +0100

    Nullable prototype for non-nullable doc is no longer disallowed

--- test/bug/10-function-doc-mixed-explicit-null-miss.php
+++ test/bug/10-function-doc-mixed-explicit-null-miss.php
@@ -3,6 +3,8 @@
  * This tests that the relatively common "computed default value" pattern works
  * without actually allowing null.
  *
+ * In other words, foo() should be allowed but foo(null) should not.
+ *
  * PHP doesn't (and won't) support this, but it is the correct expression of
  * intent.
  *
@@ -16,4 +18,10 @@ function foo(?string $bar = null) {
  */
 function bar(): string {
     return "foo";
+}
+/**
+ * Does something
+ */
+function baz() {
+    foo(null);
 }
