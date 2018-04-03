2707a87739148e48da3fee5a3922437bb617db67
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-04-03 13:04:03 +0100

    Recognise non-static "new" by looking for string values

--- src/lib/shadowtree/new.js
+++ src/lib/shadowtree/new.js
@@ -4,10 +4,11 @@ import _Node from "./node"
 import Identifier from "./identifier"
 import Variable from "./variable"
 import _Class from "./class"
+import ConstRef from "./constref"
 import {Context, ContextTypes, Doc, ParserStateOption} from "./node"
 
 export default class New extends Statement {
-    /** @type {Identifier|Variable|_Class} */
+    /** @type {Identifier|Variable|_Class|_Node} */
     get what() {
         return this.cacheNode("what")
     }
@@ -29,10 +30,24 @@ export default class New extends Statement {
         )
         if(this.what instanceof Variable) {
             return new ContextTypes(PHPSimpleType.coreTypes.mixed)
-        } else {
+        } else if(
+            (this.what instanceof Identifier) ||
+            (this.what instanceof _Class) ||
+            (this.what instanceof ConstRef)
+        ) {
             return new ContextTypes(PHPSimpleType.named(
                 context.resolveNodeName(this.what)
             ))
+        } else {
+            /** @type {string[]} */
+            let values = this.what.check(context).expressionType.coercedValues("string")
+            if(values) {
+                let type = PHPTypeUnion.empty
+                values.forEach(v => type = type.addTypesFrom(PHPSimpleType.named(v)))
+                return new ContextTypes(type)
+            } else {
+                return new ContextTypes(PHPSimpleType.coreTypes.mixed)
+            }
         }
     }
 }
