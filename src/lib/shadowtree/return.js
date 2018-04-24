2d9874425298cc484a2ab098f3a24c6d0cbaf28a
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-04-24 21:31:50 +0100

    Error for return of no value

--- src/lib/shadowtree/return.js
+++ src/lib/shadowtree/return.js
@@ -1,5 +1,6 @@
 import _Node from "./node"
 import * as PHPType from "../php-type"
+import * as PHPError from "../php-error"
 import Expression from "./expression"
 import {Context, ContextTypes, Doc, ParserStateOption} from "./node"
 export default class Return extends _Node {
@@ -17,10 +18,17 @@ export default class Return extends _Node {
     check(context, parser_state = new Set(), doc = null) {
         super.check(context, parser_state, doc)
         if(this.expr) {
-            return new ContextTypes(
-                PHPType.Union.empty,
-                this.expr.check(context, new Set(), null).expressionType
-            )
+            let return_type = this.expr.check(context, new Set(), null).expressionType
+
+            if(return_type.isEmpty) {
+                this.throw(new PHPError.AssignNoValue(), context)
+                return new ContextTypes(PHPType.Core.types.null)
+            } else {
+                return new ContextTypes(
+                    PHPType.Union.empty,
+                    return_type
+                )
+            }
         } else {
             return new ContextTypes(
                 PHPType.Union.empty,
