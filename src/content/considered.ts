2230ff253635cbeb5e04f778ac291f2f721f2010
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-09-01 11:53:28 +0100

    Simplify dispatch

--- src/content/considered.ts
+++ src/content/considered.ts
@@ -16,39 +16,26 @@ import { Closure } from "./considered/closure";
 import { New } from "./considered/new";
 import { Array } from "./considered/array";
 export { Base, Program, Echo, Function, Include };
+const byKind: {[kind: string]: typeof Base} = {
+    array: Array,
+    assign: Assign,
+    call: Call,
+    class: Class,
+    closure: Closure,
+    echo: Echo,
+    function: Function,
+    if: If,
+    include: Include,
+    namespace: Namespace,
+    new: New,
+    number: Number,
+    program: Program,
+    string: String,
+    variable: Variable,
+}
 export function forNode(node: NodeTypes.Node): Base {
-    switch(node.kind) {
-        case "array":
-            return new Array(node)
-        case "assign":
-            return new Assign(node)
-        case "call":
-            return new Call(node)
-        case "class":
-            return new Class(node)
-        case "closure":
-            return new Closure(node)
-        case "echo":
-            return new Echo(node)
-        case "function":
-            return new Function(node)
-        case "include":
-            return new Include(node)
-        case "if":
-            return new If(node)
-        case "namespace":
-            return new Namespace(node)
-        case "new":
-            return new New(node)
-        case "number":
-            return new Number(node)
-        case "program":
-            return new Program(node)
-        case "string":
-            return new String(node)
-        case "variable":
-            return new Variable(node)
-        default:
-            return new Base(node)
+    if(byKind[node.kind]) {
+        return new byKind[node.kind](node)
     }
+    return new Base(node)
 }
