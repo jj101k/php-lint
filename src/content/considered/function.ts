ce78f0a9c06159c056610d1c970df7806829e1e6
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-09-29 14:45:29 +0100

    Put functions in the namespace

--- src/content/considered/function.ts
+++ src/content/considered/function.ts
@@ -2,6 +2,7 @@ import { NodeTypes } from "../ast";
 import { Context } from "../../context";
 import { forNode, byKind } from "./for-node";
 import { Base } from "./base";
+import { Known } from "../../type/known";
 class Function extends Base {
     protected node: NodeTypes.Function
     constructor(node: NodeTypes.Function) {
@@ -15,6 +16,7 @@ class Function extends Base {
         if(this.node.body) {
             forNode(this.node.body).check(context)
         }
+        context.set(this.node.name, new Known())
         // this.node.byref
         // this.node.nullable
         return true
