1b04a2badd95ece98209a9c50dadc8a056d9ea02
Jim Driscoll <jim.a.driscoll@gmail.com>
2018-10-21 15:15:50 +0100

    Put the command-line runner in sync mode

--- src/bin.ts
+++ src/bin.ts
@@ -3,5 +3,5 @@ const PHPLint = require("./index").default
 const files = process.argv.slice(2)
 
 files.forEach(
-    file => new PHPLint().checkFile(file, 0, null)
+    file => new PHPLint().checkFileSync(file, 0, null)
 )
