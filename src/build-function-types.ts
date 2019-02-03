4b346980451bb1dfb4c9f593f3505cf46e95c533
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-02-03 12:19:43 +0000

    Avoid gzipping here, the value is negligible

--- src/build-function-types.ts
+++ src/build-function-types.ts
@@ -73,4 +73,6 @@ for(const part of parts) {
 fs.writeFileSync(
     "data/php-function-types.json.gz",
     zlib.gzipSync(JSON.stringify(function_types), "utf8")
+    "data/php-function-types.json",
+    JSON.stringify(function_types)
 )
