c5923e5fa106976d821d8cfa02a395e6542b7fb2
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-02-03 12:26:24 +0000

    PHP doc parse rewrite: get more info out of the docs

--- src/build-function-types.ts
+++ src/build-function-types.ts
@@ -1,71 +1,149 @@
 const fs = require("fs")
 const zlib = require("zlib")
+const htmlparser = require("node-html-parser")
 const data = fs.readFileSync("data-in/php-bigxhtml.html", "utf8")
-const parts = data.split(/^<hr/m)
 
-function parse_method_structure(method_structure: string): {modifier: string | null, type: string, name: string} {
-	const modifier = /(?:\s*<span class="modifier">(?:[\w\s]+)<\/span>)*/
-	const type_expr = /(?:([\w|\\?]+)|<span class="type [^"]+">([^<]+)<\/span>|<a[^>]*>(\w+)<\/a>)/
-	const method_name = /<span class="methodname">(?:<span[^>]*>([\w:\\-]+)<\/span>|<strong>([\w:\\-]+)<\/strong>|<a[^>]*>([\w:\\-]+)<\/a>)<\/span>/
-	const args = /\(([^)]*)\)/
-	const all = new RegExp(`^(${modifier.source})?\\s*(?:<span class="type">${type_expr.source}<\/span>)?\\s*${method_name.source}\\s*${args.source}`)
-	let md
-	if(md = method_structure.match(all)) {
-		const arg_xml = md[8]
-		const modifier_xml = md[1]
-		const name = md[5] || md[6] || md[7]
-		const type = md[2] || md[3] || md[4]
-		return {
-			modifier: modifier_xml ?
-				modifier_xml.replace(/<[^>]*>/g, "").replace(/^\s+/, "").replace(/\s+$/, '"') :
-				null,
-			type: type,
-			name: name,
+type htmlparserNode = {
+	text: string,
+}
+type htmlparserElement = htmlparserNode & {
+	childNodes: Array<htmlparserText | htmlparserElement>,
+	classNames: Array<string>,
+	querySelector(pattern: string): htmlparserElement | null,
+	querySelectorAll(pattern: string): Array<htmlparserElement>,
+}
+type htmlparserText = htmlparserNode & {
+
+}
+type htmlparserDocument = htmlparserElement & {
+
+}
+
+const dom: htmlparserDocument = htmlparser.parse(data)
+
+type arg_out = {defaultValue: string | null, name: string, optionalDepth: number, type: string | null}
+
+function parse_method_structure(method_structure: htmlparserElement): {args: arg_out[], modifier: string | null, type: string | null, name: string} {
+	const args_out: arg_out[] = []
+	let optional_depth = 0
+	for(const mc of method_structure.childNodes) {
+		if("classNames" in mc && mc.classNames.includes("methodparam")) {
+			const initialiser = mc.querySelector(".initializer")
+			const type = mc.querySelector(".type")
+			const name = mc.querySelector(".parameter")
+			const initialiser_value_expr = initialiser ? initialiser.text.replace(/^\s+=[\s\r\n]+/, "") : undefined
+			let initialiser_value
+			switch(initialiser_value_expr) {
+				case "FALSE":
+					initialiser_value = false
+					break
+				case "NULL":
+					initialiser_value = null
+					break
+				case "TRUE":
+					initialiser_value = true
+					break
+				case undefined:
+					// noop
+					break
+				default:
+					let md: RegExpMatchArray | null
+					if(md = initialiser_value_expr.match(/^'(.*)'$/)) {
+						initialiser_value = md[1]
+					} else if(md = initialiser_value_expr.match(/^0(\d*)$/)) {
+						initialiser_value = parseInt(md[1], 8)
+					} else if(md = initialiser_value_expr.match(/^[.](\d*)$/)) {
+						initialiser_value = parseFloat("0" + md[1])
+					} else if(md = initialiser_value_expr.match(/^(\d*)[.]$/)) {
+						initialiser_value = parseFloat(md[1])
+					} else if(initialiser_value_expr.match(/^[A-Z]/i)) {
+						console.log(`Cannot parse default value ${initialiser_value_expr}`)
+					} else {
+						try {
+							initialiser_value = JSON.parse(initialiser_value_expr)
+						} catch(e) {
+							console.log(`Misparse of default value "${initialiser_value_expr}", presuming string`)
+							initialiser_value = initialiser_value_expr
+						}
+					}
+			}
+			if(name) {
+				args_out.push({
+					defaultValue: initialiser_value,
+					name: name.text!,
+					optionalDepth: optional_depth,
+					type: type ? type.text! : null,
+				})
+			}
+		} else if(!("classNames" in mc)) {
+			optional_depth += mc.text.replace(/[^\[]/g, "").length
+			optional_depth -= mc.text.replace(/[^\]]/g, "").length
 		}
-	} else {
-		throw new Error(`Cannot parse: ${method_structure}`)
+	}
+	const modifier = method_structure.querySelector(".modifier")
+	const type = method_structure.querySelector(".type")
+	return {
+		args: args_out,
+		modifier: modifier ? modifier.text : null,
+		type: type ? type.text! : null,
+		name: method_structure.querySelector(".methodname")!.text!,
 	}
 }
-function parse_return_info(return_info: string, name: string): {orNull: boolean | null, orFalse: boolean | null} {
-	let or_false = null
-	let or_null = null
-	let md
-	if(md = return_info.match(/<p class="(?:sim)?para">([^]*)(?:<\/p>)?/)) {
-		const text_content = md[1].replace(/<[^>]*>/g, "").replace(/\n/, " ").replace(/ {2,}/g, " ")
+function parse_return_info(return_info: htmlparserElement, name: string): {orNull: boolean | null, orFalse: boolean | null} {
+	let or_false: boolean | null = null
+	let or_null: boolean | null = null
+	const info = return_info.querySelector("p.para") || return_info.querySelector("p.simpara")
+	if(info) {
+		const text_content = info.text
 		if(text_content.match(/\bor NULL\b/i)) {
 			or_null = true
 		}
 		if(text_content.match(/\bor false\b/i)) {
 			or_false = true
 		}
-	} else if(md = return_info.match(/<table/)) {
-		// Nope
 	} else {
-		throw new Error(`Cannot parse (${name}): ${return_info}`)
+		const t = return_info.querySelector(".doctable")
+		if(t) {
+			for(const td of t.querySelectorAll("td:first-child")) {
+				if(td.text == "FALSE") {
+					or_false = true
+				} else if(td.text == "NULL") {
+					or_null = true
+				}
+			}
+		} else {
+			console.log(return_info)
+			throw new Error("Cannot parse")
+		}
 	}
 	return {
 		orFalse: or_false,
 		orNull: or_null,
 	}
 }
-const function_types: {[name: string]: {returnTypes: string[]}} = {}
-for(const part of parts) {
-	let md
-	if(md = part.match(/<div class="methodsynopsis[^>]*>([^]*?)<\/div>/)) {
-		const structure = parse_method_structure(md[1])
-		if(md = part.match(/^(\s*)<div class="refsect1 returnvalues"[^>]*>([^]*?)^\1<\/div>/m)) {
-			const return_info = parse_return_info(md[2], structure.name)
-			const types = [structure.type]
+const function_types: {[name: string]: {args: arg_out[], returnTypes: string[]}} = {}
+for(const part of dom.querySelectorAll(".refentry")) {
+	const synopsis = part.querySelector(".methodsynopsis")
+	if(synopsis) {
+		const structure = parse_method_structure(synopsis)
+		const return_info_in = part.querySelector(".returnvalues")
+		if(return_info_in) {
+			const return_info = parse_return_info(return_info_in, structure.name)
+			const types = structure.type ? [structure.type] : []
 			if(return_info.orFalse) {
 				types.push("false")
 			}
 			if(return_info.orNull) {
 				types.push("null")
 			}
-			if(types.length > 1) {
-				function_types[structure.name] = {
-					returnTypes: types,
-				}
+			function_types[structure.name] = {
+				args: structure.args,
+				returnTypes: types,
+			}
+		} else {
+			function_types[structure.name] = {
+				args: structure.args,
+				returnTypes: structure.type ? [structure.type] : [],
 			}
 		}
 	}
