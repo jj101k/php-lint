b7f5839404a459117b160f40c8acba838d428ddf
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-02-17 12:20:32 +0000

    Add doc

--- src/type/inferred.ts
+++ src/type/inferred.ts
@@ -18,12 +18,26 @@ export abstract class Base extends Type.Base {
 export class ClassInstance extends Base {
     private static lastRef: number = 0
     private static refs: Map<string, number> = new Map()
+    /**
+     * Returns the name for a ref, mostly of interest during debugging.
+     *
+     * @param ref Internal reference value
+     * @returns eg. "\\Foo"
+     */
     static className(ref: number): string {
         for(const [name, i] of this.refs.entries()) {
             if(+i == ref) return name
         }
         throw new Error(`Unrecognised ref: ${ref}`)
     }
+    /**
+     * Gets the internal reference value for a class or similar (interface). If
+     * it does not already exist, it will be added. This does not create any
+     * objects for it.
+     *
+     * @param name eg. "\\Foo"
+     * @returns Internal reference value
+     */
     static classRef(name: string): number {
         if(!this.refs.get(name)) {
             this.lastRef++
