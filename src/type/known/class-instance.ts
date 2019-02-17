ccad03c0bade2cf2ae1c09bc324b8cfdc594db12
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-02-17 12:05:00 +0000

    Support type-matching with known.classinstance even where not literally identical; also fix declaration order

--- src/type/known/class-instance.ts
+++ src/type/known/class-instance.ts
@@ -7,16 +7,18 @@ import * as Inferred from "../../type/inferred"
  * A class or similar.
  */
 export class ClassInstance extends Base {
-    get shortType() {
-        return Inferred.ClassInstance.className(this.classRef)
-    }
     private classRef: number
     constructor(class_ref: number) {
         super()
         this.classRef = class_ref
     }
+    get shortType() {
+        return Inferred.ClassInstance.className(this.classRef)
+    }
     matches(type: Type.Base): boolean {
         if(type instanceof Inferred.ClassInstance) {
+            return type.classRef == this.classRef
+        } else if(type instanceof ClassInstance) {
             return type.classRef == this.classRef
         } else if(type instanceof _Class) {
             return type.ref == this.classRef
