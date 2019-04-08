60226a5fc67100fd25b2d854af8a5e4f847ccc8d
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-04-08 18:37:24 +0100

    Less pathological test

--- test/good/19-instanceof.php
+++ test/good/19-instanceof.php
@@ -3,10 +3,10 @@ $dt = (new \DateTime())->modify("x");
 if($dt instanceof \DateTime) {
     echo $dt->setDate(2000, 1, 2)->format("c");
 } else {
-    echo $dt;
+    echo $dt ?: "false";
 }
 if(is_a($dt, "DateTime")) {
     echo $dt->setDate(2000, 1, 2)->format("c");
 } else {
-    echo $dt;
+    echo $dt ?: "false";
 }
