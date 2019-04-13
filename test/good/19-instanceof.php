4c6377e6750e74f9de98015adfe10748a2fb871b
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-04-13 10:51:33 +0100

    Document some more

--- test/good/19-instanceof.php
+++ test/good/19-instanceof.php
@@ -1,12 +1,21 @@
 <?php
 $dt = (new \DateTime())->modify("x");
+// This tests type inference via instanceof
 if($dt instanceof \DateTime) {
+    // If in here, dt has a specific type (\DateTime)
     echo $dt->setDate(2000, 1, 2)->format("c");
 } else {
+    // If we're in here, because the other types have been exhausted, it's
+    // false.
     echo $dt ?: "false";
 }
+// This tests type inference via is_a.
 if(is_a($dt, "DateTime")) {
+    // If we're in here, either $dt is a class name <= DateTime, or an instance
+    // of DateTime. Since only one is supported, that should be the one we get.
     echo $dt->setDate(2000, 1, 2)->format("c");
 } else {
+    // If we're in here, because the other types have been exhausted, it's
+    // false.
     echo $dt ?: "false";
 }
